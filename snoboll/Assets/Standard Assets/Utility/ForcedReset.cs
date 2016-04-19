d719d269f1448a11980cd47af3f3c788bb7b395b
Awe <arvidvi@kth.se>
2016-04-19 14:31:03 +0200

    Added ball to game, WOHO

--- snoboll/Assets/Standard Assets/Utility/ForcedReset.cs
+++ snoboll/Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -17,24 +16,3 @@ public class ForcedReset : MonoBehaviour
         }
     }
 }
-||||||| merged common ancestors
-=======
-using System;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityStandardAssets.CrossPlatformInput;
-
-[RequireComponent(typeof (GUITexture))]
-public class ForcedReset : MonoBehaviour
-{
-    private void Update()
-    {
-        // if we have forced a reset ...
-        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
-        {
-            //... reload the scene
-            SceneManager.LoadScene(SceneManager.GetSceneAt(0).path);
-        }
-    }
-}
->>>>>>> origin/DavNick
