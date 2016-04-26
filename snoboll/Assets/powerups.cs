c6cc4201ebd18e6b55957dca0a1d828a3258be70
Awe <arvidvi@kth.se>
2016-04-26 16:32:22 +0200

    Fix powerup to fall trough map

--- snoboll/Assets/powerups.cs
+++ snoboll/Assets/powerups.cs
@@ -5,10 +5,11 @@ public class powerups : MonoBehaviour {
 
     public int rndm_time;
     public GameObject Pow;
+	private BoxCollider2D InvCollider;
 
 	// Use this for initialization
 	void Start () {
-        rndm_time = Random.Range(500, 1000);
+        rndm_time = Random.Range(0, 10);
         Pow = GameObject.Find("PowerUp_Inv");
         Pow.active = false;
 	
@@ -28,9 +29,31 @@ public class powerups : MonoBehaviour {
                 - ((2f * Camera.main.orthographicSize) / 2) + 15;
             Pow.transform.position = new Vector2(Random.Range(left, right), Random.Range(bott, top));
             Pow.active = true;
-            rndm_time = Random.Range(500, 1000);
+            rndm_time = Random.Range(0, 10);
         }
         if(!Pow.active)
             rndm_time--;
+		tel();
 	}
+	private void tel()
+	{ 
+		
+		float top = Camera.main.gameObject.transform.position.y
+			+ ((2f * Camera.main.orthographicSize) / 2) + Pow.transform.localScale.y/2;
+		float bott = Camera.main.gameObject.transform.position.y
+			- ((2f * Camera.main.orthographicSize) / 2) - Pow.transform.localScale.y/2;
+
+	
+		if (Pow.transform.position.y > top)
+		{
+			Pow.transform.position = new Vector2(Pow.transform.position.x, bott);
+
+		}
+		if (Pow.transform.position.y < bott)
+		{
+			Pow.transform.position = new Vector2(Pow.transform.position.x, top);
+
+		}
+	}
+
 }
