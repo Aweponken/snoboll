083ec2fda2c4ea01b16088c2702a9f2716f58d12
ddiamant <DavidDiamant@n154-p139.kthopen.kth.se>
2016-04-28 15:05:01 +0200

    inställningar

--- snoboll/Assets/menuScript.cs
+++ snoboll/Assets/menuScript.cs
@@ -64,7 +64,7 @@ public class menuScript : MonoBehaviour
 		GameWideScript.Instance.setSound = Sound;
 		GameWideScript.Instance.setPow = Pow;
 		GameWideScript.Instance.setZone = Zone;
-		GameWideScript.Instance.setTime = Time;
+		GameWideScript.Instance.setTime = 10 * Time;
 		GameWideScript.Instance.setPlayers = Players;
 		GameWideScript.Instance.setCostum = true;
 
@@ -92,7 +92,7 @@ public class menuScript : MonoBehaviour
 	public void playersChange(){
 		Players = (int)players.value;
 		playersValue.text = Players.ToString();
-		switch (Players) {
+		/*switch (Players) {
 		case 2:
 			mapChoice (1);
 			break;
@@ -102,7 +102,7 @@ public class menuScript : MonoBehaviour
 		case 4:
 			mapChoice (3);
 			break;
-		}
+		}*/
 	}
 
 	public void LoadHowToPlay () {
@@ -164,10 +164,13 @@ public class menuScript : MonoBehaviour
 
 	public void onClickSound() {
 		Sound = !Sound;
-		if(!Sound) 
+		if (!Sound) { 
 			SoundButton.GetComponent<Image> ().color = Color.white;
-		else
+			AudioListener.pause = true;
+		} else {
 			SoundButton.GetComponent<Image> ().color = Color.gray;
+			AudioListener.pause = false;
+		}
 	}
 
 	public void onClickPower() {
--- snoboll/Assets/powerups.cs
+++ snoboll/Assets/powerups.cs
@@ -32,47 +32,41 @@ public class powerups : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         
-        if (rndm_time <= 0 && !(Pow.active))
-        {
-            if (a == 1)
-            {
-                Pow1.active = true;
-                Pow = Pow1;
+		if (rndm_time <= 0 && !(Pow.active)) {
+			if (GameWideScript.Instance.setPow) {
+				if (a == 1) {
+					Pow1.active = true;
+					Pow = Pow1;
 
-            }
-            else if (a == 2)
-            {
-                Pow2.active = true;
-                Pow = Pow2;
-            }
+				} else if (a == 2) {
+					Pow2.active = true;
+					Pow = Pow2;
+				} else if (a == 3) {
+					Pow3.active = true;
+					Pow = Pow3;
+				} else {
+					Pow4.active = true;
+					Pow = Pow4;
+				}
+				float left = Camera.main.gameObject.transform.position.x
+				                     - ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) + 100;
+				float right = Camera.main.gameObject.transform.position.x
+				                      + ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) - 100;
+				float top = Camera.main.gameObject.transform.position.y
+				                    + ((2f * Camera.main.orthographicSize) / 2) - 15;
+				float bott = Camera.main.gameObject.transform.position.y
+				                     - ((2f * Camera.main.orthographicSize) / 2) + 15;
+				Pow.transform.position = new Vector2 (Random.Range (left, right), Random.Range (bott, top));
+				Pow.active = true;
+				rndm_time = Random.Range (100, 200);
 
-            else if (a == 3)
-            {
-                Pow3.active = true;
-                Pow = Pow3;
-            }
-            else
-            {
-                Pow4.active = true;
-                Pow = Pow4;
-            }
-            float left = Camera.main.gameObject.transform.position.x
-                 - ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) + 100;
-            float right = Camera.main.gameObject.transform.position.x
-                + ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) - 100;
-            float top = Camera.main.gameObject.transform.position.y
-                + ((2f * Camera.main.orthographicSize) / 2) - 15;
-            float bott = Camera.main.gameObject.transform.position.y
-                - ((2f * Camera.main.orthographicSize) / 2) + 15;
-            Pow.transform.position = new Vector2(Random.Range(left, right), Random.Range(bott, top));
-            Pow.active = true;
-            rndm_time = Random.Range(100, 200);
-
-        }
+			}
+		}
         a = Random.Range(1, 5);
         if(!Pow.active)
             rndm_time--;
 		tel();
+
 	}
 	private void tel()
 	{ 
