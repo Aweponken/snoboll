68357f7dc0908d7a7695a35340d5bb4a283dd1cc
Awe <arvidvi@kth.se>
2016-05-03 16:18:45 +0200

    Music working

---
+++ snoboll/Assets/Sound/MusicController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+	static private bool menuIsActive = true;
+	AudioSource menu;
+	AudioSource game;
+
+	void Awake()
+	{
+		if (menuIsActive) {
+			AudioSource[] audios = GetComponents<AudioSource>(); //Hämta alla ljudfiler
+			menu = audios[0]; 
+			game = audios[1]; 
+			menu.Play ();
+			DontDestroyOnLoad (gameObject);
+		} 
+	}
+	void Update () {
+		if(Application.loadedLevelName == "Map" || Application.loadedLevelName == "Map2" || Application.loadedLevelName == "Map3")
+		{
+			menu.loop = false;
+			if (!menu.isPlaying  && menuIsActive) {
+				game.loop = true;
+				game.Play ();
+				menuIsActive = false;
+			}	
+		}
+		if(Application.loadedLevelName == "Options")
+		{
+			game.loop = false;
+			if (!game.isPlaying && !menuIsActive) {
+				menu.loop = true;
+				menu.Play ();
+				menuIsActive = true;
+			}	
+		} 
+	}
+}
---
+++ snoboll/Assets/Sound/SoundFXController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundFXController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioSource FX = GetComponent<AudioSource>(); 
+	}
+
+	/*void OnCollisionEnter2D(Collider coll) {
+		if (coll.tag == "Boll")
+			print ("AJAJ");
+	}*/
+}
