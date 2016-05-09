a72f39304a7a26d27a5540a2a2690fb97949e7ef
hamham <hamham@kth.se>
2016-05-09 16:18:46 +0200

    Merge fix

--- snoboll/Assets/Lightning.cs
+++ snoboll/Assets/Lightning.cs
@@ -1,18 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Lightning : MonoBehaviour {
+public class Lightning : MonoBehaviour
+{
 
     public static int count = 0;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        count++;
-        if ( count >= 200 )
-            
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
 }
