7ef31e11189e36ffbd4fbcbf65b75800faaf4e2f
Awe <arvidvi@kth.se>
2016-05-12 14:21:50 +0200

    Updated Powerup files

--- snoboll/Assets/PowerUp_Inv_scr.cs
+++ snoboll/Assets/PowerUp_Inv_scr.cs
@@ -3,13 +3,14 @@ using System.Collections;
 /// <summary>
 /// script for invert-power up
 /// </summary>
+ 
 public class PowerUp_Inv_scr : MonoBehaviour {
 
-	GameObject inv3d;
-	// Use this for initialization
+	AudioClip FX;
+
 	void Start () 
     {
-		inv3d = GameObject.Find("powInv");
+		FX = GetComponent<AudioSource> ().clip;
         float left = Camera.main.gameObject.transform.position.x
                  - ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) + 100;
         float right = Camera.main.gameObject.transform.position.x
@@ -20,56 +21,21 @@ public class PowerUp_Inv_scr : MonoBehaviour {
             - ((2f * Camera.main.orthographicSize) / 2) + 15;
         gameObject.transform.position = new Vector2(Random.Range(left, right), Random.Range(bott, top));
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () 
-    {
-		inv3d.transform.position = new Vector3 (transform.position.x+12.5f,transform.position.y+12.5f,30);
-	}
 
     void OnCollisionEnter2D(Collision2D coll)
-    {
-		
-        if (coll.gameObject.name == "Boll")
+    {		
+        if (coll.gameObject.tag == "Boll")
         {
-            GameObject snoBoll = GameObject.Find("Boll");
-            SnoBoll script = (SnoBoll)snoBoll.GetComponent(typeof(SnoBoll));
-            script.inv();
-			inv3d.transform.position = new Vector3 (-30,-30,-30); 
+			AudioSource.PlayClipAtPoint (FX, new Vector2(0,0));
+			coll.gameObject.SendMessage ("inv");
 			gameObject.SetActive(false);
         }
-        else if (coll.gameObject.name == "Boll 2")
-        {
-            GameObject snoBoll = GameObject.Find("Boll 2");
-            SnoBoll2 script = (SnoBoll2)snoBoll.GetComponent(typeof(SnoBoll2));
-            script.inv();
-			inv3d.transform.position = new Vector3 (-30,-30,-30); 
-			gameObject.SetActive(false);
-        }
-		else if (coll.gameObject.name == "Boll 3")
-		{
-			print ("BOLL 3");
-			GameObject snoBoll = GameObject.Find("Boll 3");
-			SnoBoll3 script = (SnoBoll3)snoBoll.GetComponent(typeof(SnoBoll3));
-			script.inv();
-			inv3d.transform.position = new Vector3 (-30,-30,-30); 
-			gameObject.SetActive(false);
-		}
-		else if (coll.gameObject.name == "Boll 4")
-		{
-			print ("Boll 4");
-			GameObject snoBoll = GameObject.Find("Boll 4");
-			SnoBoll4 script = (SnoBoll4)snoBoll.GetComponent(typeof(SnoBoll4));
-			script.inv();
-			inv3d.transform.position = new Vector3 (-30,-30,-30); 
-			gameObject.SetActive(false);
-		}
     }
+
     IEnumerator wfs1()
     {
         SnoBoll.PowerUp_Inv = true;
         yield return new WaitForSeconds(5);
         SnoBoll.PowerUp_Inv = false;
     }
-    
 }
--- snoboll/Assets/PowerUp_Shield.cs
+++ snoboll/Assets/PowerUp_Shield.cs
@@ -6,62 +6,29 @@ using System.Collections;
 /// </summary>
 public class PowerUp_Shield : MonoBehaviour
 {
-	GameObject shield3d;
-    // Use this for initialization
-    void Start()
-    {
-		shield3d = GameObject.Find("powShield");
-		Debug.Log ("pos "+shield3d.transform.position.x);
-        float left = Camera.main.gameObject.transform.position.x
-                 - ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) + 100;
-        float right = Camera.main.gameObject.transform.position.x
-            + ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) - 100;
-        float top = Camera.main.gameObject.transform.position.y
-            + ((2f * Camera.main.orthographicSize) / 2) - 15;
-        float bott = Camera.main.gameObject.transform.position.y
-            - ((2f * Camera.main.orthographicSize) / 2) + 15;
-        gameObject.transform.position = new Vector2(Random.Range(left, right), Random.Range(bott, top));
+	AudioClip FX;
 
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-		shield3d.transform.position = new Vector3 (transform.position.x+12.5f,transform.position.y+12.5f,30);
-    }
+	void Start()
+	{
+		FX = GetComponent<AudioSource> ().clip;
+		float left = Camera.main.gameObject.transform.position.x
+			- ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) + 100;
+		float right = Camera.main.gameObject.transform.position.x
+			+ ((Camera.main.aspect * 2f * Camera.main.orthographicSize) / 2) - 100;
+		float top = Camera.main.gameObject.transform.position.y
+			+ ((2f * Camera.main.orthographicSize) / 2) - 15;
+		float bott = Camera.main.gameObject.transform.position.y
+			- ((2f * Camera.main.orthographicSize) / 2) + 15;
+		gameObject.transform.position = new Vector2(Random.Range(left, right), Random.Range(bott, top));
+	}
+		
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.name == "Boll")
-        {
-            GameObject snoBoll = GameObject.Find("Boll");
-            SnoBoll script = (SnoBoll)snoBoll.GetComponent(typeof(SnoBoll));
-            script.setShield();
-			shield3d.transform.position = new Vector3 (-30,-30,-30); 
+		if (coll.gameObject.tag == "Boll")
+		{
+			AudioSource.PlayClipAtPoint (FX, new Vector2(0,0));
+			coll.gameObject.SendMessage ("setShield");
 			gameObject.SetActive(false);
-        }
-		else if (coll.gameObject.name == "Boll 2")
-        {
-            GameObject snoBoll = GameObject.Find("Boll 2");
-            SnoBoll2 script = (SnoBoll2)snoBoll.GetComponent(typeof(SnoBoll2));
-            script.setShield();
-			shield3d.transform.position = new Vector3 (-30,-30,-30); 
-			gameObject.SetActive(false);
-        }
-		else if (coll.gameObject.name == "Boll 3")
-        {
-            GameObject snoBoll = GameObject.Find("Boll 3");
-            SnoBoll3 script = (SnoBoll3)snoBoll.GetComponent(typeof(SnoBoll3));
-            script.setShield();
-			shield3d.transform.position = new Vector3 (-30,-30,-30); 
-			gameObject.SetActive(false);
-        }
-		else if (coll.gameObject.name == "Boll 4")
-        {
-            GameObject snoBoll = GameObject.Find("Boll 4");
-            SnoBoll4 script = (SnoBoll4)snoBoll.GetComponent(typeof(SnoBoll4));
-            script.setShield();
-			shield3d.transform.position = new Vector3 (transform.position.x+transform.localScale.x,transform.position.y,-30); 
-			gameObject.SetActive(false);
-        }
+		}
     }
 }
