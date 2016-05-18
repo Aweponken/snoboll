fd1ca942614a8442ad179abd34a87fb0e0c71c5a
ddiamant <DavidDiamant@n129-p190.kthopen.kth.se>
2016-05-18 12:55:50 +0200

    BuggfixMedMenyn

--- snoboll/Assets/BollMenyScript.cs
+++ snoboll/Assets/BollMenyScript.cs
@@ -103,40 +103,46 @@ public class BollMenyScript : MonoBehaviour {
 		if (fadeInBalls) {
 			if (removeCounter == 13) {
 				gameObject.GetComponent<CanvasGroup> ().interactable = true;
-				Debug.Log (GameWideScript.colorSelection.isPlayedOnce);
 				if (GameWideScript.colorSelection.isPlayedOnce) {
 					player1I = GameWideScript.colorSelection.playersIndex[0];
 					player2I = GameWideScript.colorSelection.playersIndex[1];
 					player3I = GameWideScript.colorSelection.playersIndex[2];
 					player4I = GameWideScript.colorSelection.playersIndex[3];
 					selectedColors = GameWideScript.colorSelection.selectedColors;
-					isSelected = GameWideScript.colorSelection.isSelected;
-					playersSelectedColor = GameWideScript.colorSelection.playersSelectedColor;
-
+					bool[] temp1 = new bool[GameWideScript.Instance.setPlayers];
+					for(int i = 0; i < GameWideScript.colorSelection.isSelected.Length; i++)
+						temp1[i] = GameWideScript.colorSelection.isSelected[i];
+					isSelected = temp1; 
+					int[] temp2 = new int[GameWideScript.Instance.setPlayers];
+					for (int i = 0; i < GameWideScript.colorSelection.playersSelectedColor.Length; i++)
+						temp2 [i] = GameWideScript.colorSelection.playersSelectedColor [i];
+					playersSelectedColor = temp2;
 					selectColor (1, Mod(player1I,selectedColors.Length));
 					selectColor (2, Mod(player2I,selectedColors.Length));
-					if (isSelected.Length > 2) {
-						selectColor (3, Mod (player3I, selectedColors.Length));
-						if(isSelected.Length > 3)
+					if (GameWideScript.Instance.setPlayers > 2) {
+						if (isSelected [2])
+							selectColor (3, Mod (player3I, selectedColors.Length));
+						if (GameWideScript.Instance.setPlayers > 3) 
+						if (isSelected [3])
 							selectColor (4, Mod (player4I, selectedColors.Length));
 					}
 
 					int witchPlayer = 0;
-					foreach (int x in playersSelectedColor) {
+					foreach (int x in GameWideScript.colorSelection.playersSelectedColor) {
 						GameObject[] temp = null;
 						switch (witchPlayer) {
-							case 0: 
-								temp = player1Balls;
-								break;
-							case 1: 
-								temp = player2Balls;
-								break;
-							case 2: 
-								temp = player3Balls;
-								break;
-							case 3: 
-								temp = player4Balls;
-								break;
+						case 0: 
+							temp = player1Balls;
+							break;
+						case 1: 
+							temp = player2Balls;
+							break;
+						case 2: 
+							temp = player3Balls;
+							break;
+						case 3: 
+							temp = player4Balls;
+							break;
 						}
 						if (x < 3) {
 							int middle = 3;
@@ -144,7 +150,7 @@ public class BollMenyScript : MonoBehaviour {
 								StartCoroutine (rotateRightNoDelay (middle, temp));
 								middle -= 1;
 							}
-						} else if (x > 3) {
+						}  else if (x > 3) {
 							int middle = 3;
 							for (int rotate = (x - 3); rotate > 0; rotate--){
 								StartCoroutine (rotateLeftNoDelay (middle, temp));
@@ -153,7 +159,7 @@ public class BollMenyScript : MonoBehaviour {
 						}
 						witchPlayer += 1;
 					}
-				} else {
+				}  else {
 					isSelected = new bool[GameWideScript.Instance.setPlayers];
 					playersSelectedColor = new int[GameWideScript.Instance.setPlayers];
 				}
@@ -191,10 +197,8 @@ public class BollMenyScript : MonoBehaviour {
 			if (Input.GetAxis ("Horizontal1") == 1 && !player1Left) {
 				if (!player1Right) {
 					player1Right = !player1Right;
-					StartCoroutine (rotateRight (player1I, player1Balls));
-					player1I -= 1;
-					if(isSelected [0])
-						isSelected [0] = false;
+					StartCoroutine (rotateLeft (player1I, player1Balls));
+					player1I += 1;
 				}
 			} else if (Input.GetAxis ("Horizontal1") < 0.8 && player1Right) {
 				player1Right = !player1Right;
@@ -203,20 +207,20 @@ public class BollMenyScript : MonoBehaviour {
 			if (Input.GetAxis ("Horizontal1") < -0.9 && !player1Right) {
 				if (!player1Left) {
 					player1Left = !player1Left;
-					StartCoroutine (rotateLeft (player1I, player1Balls));
-					player1I += 1;
-					if(isSelected [0])
-						isSelected [0] = false;
+					StartCoroutine (rotateRight (player1I, player1Balls));
+					player1I -= 1;
 				}
-			} else if (Input.GetAxis ("Horizontal1") > -0.8 && player1Left) {
+			} else if (Input.GetAxis ("Horizontal1") > -0.7 && player1Left) {
 				player1Left = !player1Left;
 			}
 
 			if (Input.GetAxis ("Jump1") == 1) {
 				if (!player1Clicked) {
 					player1Clicked = !player1Clicked;
-					if(!selectedColors[Mod(player1I,selectedColors.Length)])
-						selectColor (1, Mod(player1I,selectedColors.Length));
+					if (!selectedColors [Mod (player1I, selectedColors.Length)]) {
+						isSelected [0] = false;
+						selectColor (1, Mod (player1I, selectedColors.Length));
+					}
 				}
 			} else if (Input.GetAxis ("Jump1") == 0 && player1Clicked) {
 				player1Clicked = !player1Clicked;
@@ -226,10 +230,8 @@ public class BollMenyScript : MonoBehaviour {
 			if (Input.GetAxis ("Horizontal2") == 1 && !player2Left) {
 				if (!player2Right) {
 					player2Right = !player2Right;
-					StartCoroutine (rotateRight (player2I, player2Balls));
-					player2I -= 1;
-					if(isSelected [1])
-						isSelected [1] = false;
+					StartCoroutine (rotateLeft (player2I, player2Balls));
+					player2I += 1;
 				}
 			} else if (Input.GetAxis ("Horizontal2") < 0.8 && player2Right) {
 				player2Right = !player2Right;
@@ -238,19 +240,19 @@ public class BollMenyScript : MonoBehaviour {
 			if (Input.GetAxis ("Horizontal2") <-0.9 && !player2Right) {
 				if (!player2Left) {
 					player2Left = !player2Left;
-					StartCoroutine (rotateLeft (player2I, player2Balls));
-					player2I += 1;
-					if(isSelected [1])
-						isSelected [1] = false;
+					StartCoroutine (rotateRight (player2I, player2Balls));
+					player2I -= 1;
 				}
-			} else if (Input.GetAxis ("Horizontal2") > -0.8 && player2Left) {
+			} else if (Input.GetAxis ("Horizontal2") > -0.7 && player2Left) {
 				player2Left = !player2Left;
 			}
 			if (Input.GetAxis ("Jump2") == 1) {
 				if (!player2Clicked) {
 					player2Clicked = !player2Clicked;
-					if(!selectedColors[Mod(player2I,selectedColors.Length)])
-						selectColor (2, Mod(player2I,selectedColors.Length));
+					if (!selectedColors [Mod (player2I, selectedColors.Length)]) {
+						isSelected [1] = false;
+						selectColor (2, Mod (player2I, selectedColors.Length));
+					}
 				}
 			} else if (Input.GetAxis ("Jump2") == 0 && player2Clicked) {
 				player2Clicked = !player2Clicked;
@@ -261,10 +263,8 @@ public class BollMenyScript : MonoBehaviour {
 				if (Input.GetAxis ("Horizontal3") == 1 && !player3Left) {
 					if (!player3Right) {
 						player3Right = !player3Right;
-						StartCoroutine (rotateRight (player3I, player3Balls));
-						player3I -= 1;
-						if (isSelected [2])
-							isSelected [2] = false;
+						StartCoroutine (rotateLeft (player3I, player3Balls));
+						player3I += 1;
 					}
 				} else if (Input.GetAxis ("Horizontal3") < 0.8 && player3Right) {
 					player3Right = !player3Right;
@@ -273,19 +273,19 @@ public class BollMenyScript : MonoBehaviour {
 				if (Input.GetAxis ("Horizontal3") <-0.9 && !player3Right) {
 					if (!player3Left) {
 						player3Left = !player3Left;
-						StartCoroutine (rotateLeft (player3I, player3Balls));
-						player3I += 1;
-						if (isSelected [2])
-							isSelected [2] = false;
+						StartCoroutine (rotateRight (player3I, player3Balls));
+						player3I -= 1;
 					}
-				} else if (Input.GetAxis ("Horizontal3") > -0.8 && player3Left) {
+				} else if (Input.GetAxis ("Horizontal3") > -0.7 && player3Left) {
 					player3Left = !player3Left;
 				}
 				if (Input.GetAxis ("Jump3") == 1) {
 					if (!player3Clicked) {
 						player3Clicked = !player3Clicked;
-						if (!selectedColors [Mod (player3I, selectedColors.Length)])
+						if (!selectedColors [Mod (player3I, selectedColors.Length)]) {
+							isSelected [2] = false;
 							selectColor (3, Mod (player3I, selectedColors.Length));
+						}
 					}
 				} else if (Input.GetAxis ("Jump3") == 0 && player3Clicked) {
 					player3Clicked = !player3Clicked;
@@ -296,31 +296,29 @@ public class BollMenyScript : MonoBehaviour {
 					if (Input.GetAxis ("Horizontal4") == 1 && !player4Left) {
 						if (!player4Right) {
 							player4Right = !player4Right;
-							StartCoroutine (rotateRight (player4I, player4Balls));
-							player4I -= 1;
-							if (isSelected [3])
-								isSelected [3] = false;
+							StartCoroutine (rotateLeft (player4I, player4Balls));
+							player4I += 1;
 						}
-					} else if (Input.GetAxis ("Horizontal4") < 0.8 && player4Right) {
+					} else if (Input.GetAxis ("Horizontal4") < 0.7 && player4Right) {
 						player4Right = !player4Right;
 					}
 
 					if (Input.GetAxis ("Horizontal4") <-0.9 && !player4Right) {
 						if (!player4Left) {
 							player4Left = !player4Left;
-							StartCoroutine (rotateLeft (player4I, player4Balls));
-							player4I += 1;
-							if (isSelected [3])
-								isSelected [3] = false;
+							StartCoroutine (rotateRight (player4I, player4Balls));
+							player4I -= 1;
 						}
-					} else if (Input.GetAxis ("Horizontal4") > -0.8 && player4Left) {
+					} else if (Input.GetAxis ("Horizontal4") > -0.7 && player4Left) {
 						player4Left = !player4Left;
 					}
 					if (Input.GetAxis ("Jump4") == 1) {
 						if (!player4Clicked) {
 							player4Clicked = !player4Clicked;
-							if (!selectedColors [Mod (player4I, selectedColors.Length)])
+							if (!selectedColors [Mod (player4I, selectedColors.Length)]) {
+								isSelected [3] = false;
 								selectColor (4, Mod (player4I, selectedColors.Length));
+							}
 						}
 					} else if (Input.GetAxis ("Jump4") == 0 && player4Clicked) {
 						player4Clicked = !player4Clicked;
